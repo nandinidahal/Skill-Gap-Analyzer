6b64144ba4b4f1a935076ac5122c3e8640a5909e
Nandini Dahal <bidishadahal338@gmail.com>
2026-05-20 11:57:45 +0545

    contact us update with cross sign

--- Frontened/src/pages/component/Contactus.jsx
+++ Frontened/src/pages/component/Contactus.jsx
@@ -1,18 +1,21 @@
-import React, { useState } from "react";
-import Navbar from "../../components/navbar/Navbar";
-import Footer from "../../components/footer/Footer";
+import { useState } from "react";
+// import Navbar from "../../components/navbar/Navbar";
+// import Footer from "../../components/footer/Footer";
 // import { IoMailOpenOutline } from "react-icons/io5";
 // import { CiLocationOn } from "react-icons/ci";
 // import { CiLocationOn } from "react-icons/ci";
 import { IoMdMailUnread } from "react-icons/io";
 import { IoLocationSharp } from "react-icons/io5";
 import { MdOutlineSmartphone } from "react-icons/md";
+import { RxCross2 } from "react-icons/rx";
+import { useNavigate } from "react-router-dom";
 
 function Contactus() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [numb, setNumb] = useState("");
   const [message, setMessage] = useState("");
+  const navigate = useNavigate();
   function handleSubmit(e) {
     e.preventDefault();
     let formIsValid = true;
@@ -44,19 +47,27 @@ function Contactus() {
       alert(Error);
     }
   }
+  const handelClick = () => {
+    navigate("/");
+  };
 
   return (
     <>
       {/* <Navbar /> */}
       <div>
-        <div className=" p-4 rounded-xl shadow cursor-pointer">
+        <div className=" p-4 rounded-xl  cursor-pointer flex justify-center mt-4">
           <form
             onSubmit={handleSubmit}
-            className="max-w-4xl mx-auto  p-6 md:p-10 "
+            className="relative max-w-2xl border p-6 md:p-10 shadow-md hover:shadow-inner rounded-lg mt-2"
           >
-            <h2 className="text-2xl md:text-3xl font-bold text-center text-blue-600  mb-8 ">
-              Contact 
-              <span className="text-blue-800"> Us</span>
+            <RxCross2
+              size={22}
+              className="absolute top-4 right-4 cursor-pointer hover:text-red-500 transition"
+              onClick={handelClick}
+            />
+
+            <h2 className="text-2xl md:text-3xl font-bold text-center text-blue-600">
+              Contact <span className="text-blue-800">Us</span>
             </h2>
             <p className="text-white  text-center mb-8">
               Any questions or remarks? Just write us a message!
@@ -137,9 +148,7 @@ function Contactus() {
               <h4 className="text-sm text-white font-bold  uppercase tracking-widest mb-2">
                 Email Us
               </h4>
-              <p className="text-white text-sm ">
-                skillgapanalyzer@gmail.com
-              </p>
+              <p className="text-white text-sm ">skillgapanalyzer@gmail.com</p>
             </div>
             <div className="flex flex-col justify-center items-center">
               <MdOutlineSmartphone className="text-4xl text-white mb-3 " />
@@ -150,7 +159,7 @@ function Contactus() {
               <p className="text-white text-sm">+977 9800000000</p>
             </div>
             <div className="flex flex-col justify-center items-center">
-              <IoLocationSharp  className="text-4xl text-white mb-3"/>
+              <IoLocationSharp className="text-4xl text-white mb-3" />
               <h4 className="text-sm font-bold text-white uppercase tracking-widest mb-2">
                 Our Office Location
               </h4>
